6157ec212c390d842031614a2fcf84549e54b525
Jayce Azua <jayce.azua@gmail.com>
2018-10-07 00:03:34 -0700

    done finally

--- shoppingCart.js
+++ shoppingCart.js
@@ -3,39 +3,95 @@ function Item (name, price) {
     // creating a new item.
     this.name = name;
     this.price = price;
-    this.qty = 0;
+    this.qty = 0 + 1;
     this.cost = 0
 }
-// increate qty
-Object.defineProperty(Item, "increaseQty", {
-    get: function() { this.qty += 1 }
-});
-// decrease qty
-Object.defineProperty(Item, "decreaseQty", {
-    get: function() { this.qty -= 1 }
-});
-// calculate total cost
-Object.defineProperty(Item, "calcCost", {
-    get: function() { return this.qty * this.price }
-});
 
 class ShoppingCart {
     constructor() {
         this.cart = []
     };
-    set addItem(Item) {
-        this.cart.push(Item);
+
+    set addItem(_item) {
+        const cart = this.cart;
+        if (this.howManyItems > 0) {
+            if (this.findItem()(_item)) {
+                cart.forEach((item) => {
+                    if(item.name === _item.name){
+                        item.qty += 1
+                    }
+                })
+                return
+            }
+            else {
+                cart.push(_item)
+            }
+        }
+        else {
+            cart.push(_item)
+        }
+
     };
+
     get howManyItems() {
         return this.cart.length
     };
-    get remove() {
-        return this.cart = []
 
+    get removeAllItem() {
+        return this.cart = []
     };
+
+    findItem() {
+        const cart = this.cart;
+        // closure function
+        return function(_item) {
+            for(let i = 0; i < cart.length; i += 1) {
+                if (_item.name === cart[i].name) {
+                    return true
+                }
+            }
+            return false
+        }
+    };
+    removeItem(_item) {
+        const cart = this.cart;
+        if (this.findItem()(_item)) {
+            cart.forEach((item) => {
+                if(item.name === _item.name){
+                    item.qty -= 1
+                    this.removeItemsWithZero()
+                }
+            })
+        }
+        return
+    }
+    removeItemsWithZero() {
+        const cart = this.cart;
+        cart.forEach((item) => {
+            if(item.qty === 0) {
+                let itemToRemove = cart.indexOf(item)
+                cart.splice(itemToRemove, 1);
+                return
+            }
+        })
+    }
     get totalQty() {};
     get totalCost() {}
 }
+
+
+// test
+let new_item_1 = new Item('apple', 0.99);
+let new_item_2 = new Item('orange', 0.79);
+let new_item_3 = new Item('pineapple', 1.99);
+let new_item_4 = new Item('orange', 0.79);
+let shoppingCart = new ShoppingCart();
+shoppingCart.addItem = new_item_1
+shoppingCart.addItem = new_item_2
+shoppingCart.addItem = new_item_3
+shoppingCart.addItem = new_item_4
+
+console.log(shoppingCart.cart)
 
 module.exports = {
     Item,
--- test/shoppingCart_test.js
+++ test/shoppingCart_test.js
@@ -3,9 +3,10 @@ const expect = chai.expect;
 const { Item,  ShoppingCart } = require('../shoppingCart')
 
 it('Should create a new item with name and price', () => {
-    let new_item = new Item('apple', 0.99);
-    expect(new_item.name).to.equal('apple');
-    expect(new_item.price).to.equal(0.99);
+    let new_item_1 = new Item('apple', 0.99);
+    let new_item_2 = new Item('apple', 0.99);
+    expect(new_item_1.name).to.equal('apple');
+    expect(new_item_1.price).to.equal(0.99);
 });
 
 it('Should return an array containing all items in cart', () => {
@@ -46,24 +47,47 @@ it('Should remove items from cart', () => {
     let new_item_2 = new Item('orange', 0.79);
     let new_item_3 = new Item('pineapple', 1.99);
     let shoppingCart = new ShoppingCart();
-    shoppingCart.remove
+    shoppingCart.removeAllItem
     expect(shoppingCart.howManyItems).to.equal(0)
 });
 
 // Stretch challenges
 it('Should update the count of items in the cart', () => {
-    // what do you mean the count of items in the cart?
-    // do you mean the qty of each item?
+    // what do you mean "return" an array?
+    let new_item_1 = new Item('apple', 0.99);
+    let new_item_2 = new Item('orange', 0.79);
+    let new_item_3 = new Item('pineapple', 1.99);
+    let new_item_4 = new Item('pineapple', 1.99);
+    let new_item_5 = new Item('orange', 0.79);
+    let shoppingCart = new ShoppingCart();
+    shoppingCart.addItem = new_item_1
+    shoppingCart.addItem = new_item_2
+    shoppingCart.addItem = new_item_3
+    shoppingCart.addItem = new_item_4
+    shoppingCart.addItem = new_item_5
+    expect(new_item_2.qty).to.equal(2)
+    expect(new_item_3.qty).to.equal(2)
+    expect(shoppingCart.cart).to.be.a('array');
+    expect(shoppingCart.cart.length).to.equal(3);
 });
 
 it('Should remove an item when its count is 0', () => {
     // Do not understand if remove all or just one item.
     let new_item_1 = new Item('apple', 0.99);
-    let new_item_2 = new Item('orange', 0.79);
+    let new_item_2 = new Item('apple', 0.79);
     let new_item_3 = new Item('pineapple', 1.99);
     let shoppingCart = new ShoppingCart();
-    // method to remove 1 qty of a specific item
+    shoppingCart.addItem = new_item_1
+    shoppingCart.addItem = new_item_2
+    shoppingCart.addItem = new_item_3
+    expect(new_item_1.qty).to.equal(2)
     expect(shoppingCart.howManyItems).to.equal(2)
+    shoppingCart.removeItem(new_item_1)
+    shoppingCart.removeItem(new_item_1)
+    shoppingCart.removeItem(new_item_1)
+    expect(new_item_1.qty).to.equal(0)
+    // method to remove 1 qty of a specific item
+    expect(shoppingCart.howManyItems).to.equal(1)
 });
 
 it('Should return the total cost of all items in the cart', () => {
